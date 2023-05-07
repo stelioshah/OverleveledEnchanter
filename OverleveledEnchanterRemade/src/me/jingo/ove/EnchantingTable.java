d56214efdffb90e183716b0abeb984858fd4fcd2
stelioshah <stelioshah@gmail.com>
2023-05-07 11:24:39 +0300

    Minor fixes

--- OverleveledEnchanterRemade/src/me/jingo/ove/EnchantingTable.java
+++ OverleveledEnchanterRemade/src/me/jingo/ove/EnchantingTable.java
@@ -43,16 +43,18 @@ public class EnchantingTable implements Listener{
 		
 		for(EnchantmentOffer theOffer : theTable.getOffers()) {
 			
-				int maxLevel = pluginInstance.getMaxLevel(theOffer.getEnchantment());
-				
-				if(pluginInstance.isLevelInvalid(maxLevel)) {
-					maxLevel = theOffer.getEnchantment().getMaxLevel();
-					String.format(OverleveledEnchanter.noMaxLevelFoundWarn, theOffer.getEnchantment().getKey().getKey(), theOffer.getEnchantment().getMaxLevel());
-				}
-				
-				if(theOffer.getEnchantmentLevel() > maxLevel && !pluginInstance.hasNoMaxLevel(maxLevel))
-					theOffer.setEnchantmentLevel(maxLevel);
+			if(theOffer == null) continue;
 			
+			int maxLevel = pluginInstance.getMaxLevel(theOffer.getEnchantment());
+			
+			if(pluginInstance.isLevelInvalid(maxLevel)) {
+				maxLevel = theOffer.getEnchantment().getMaxLevel();
+				String.format(OverleveledEnchanter.noMaxLevelFoundWarn, theOffer.getEnchantment().getKey().getKey(), theOffer.getEnchantment().getMaxLevel());
+			}
+			
+			if(theOffer.getEnchantmentLevel() > maxLevel && !pluginInstance.hasNoMaxLevel(maxLevel))
+				theOffer.setEnchantmentLevel(maxLevel);
+		
 		}
 		
 	}
